98a32048974648e7101ac3cd63b6d81108617704
Khoroshevskyi <sasha99250@gmail.com>
2019-12-15 17:18:45 +0100

    list array complete and marge

--- baza.py
+++ baza.py
@@ -1,11 +1,33 @@
+import ast
+import numpy as np
+import os
+
+
 def open_dict(file_name):
-    data = open(file_name,"r")
+    all_arrays = []
+    for filename in os.listdir("E://Przyrod-master//1//1.1-pracownia inform//git//all_words"):
+        true_arr_list1 = xml_to_list_dict("E://Przyrod-master//1//1.1-pracownia inform//git//all_words//"+filename)
+        all_arrays = np.concatenate((all_arrays, true_arr_list1), axis=0)
+    #all_arrays.append(true_arr_list1[:])
+    #all_arrays.append(true_arr_list2[:])
+
+
+    #for k in all_arrays:
+        #print(k)
+    #write_to_file(true_arr_list, 'dictionary.txt')
+    #meaning_list = read_finished_file('dictionary.txt')
+    #for k in meaning_list:
+        #print(k)
+
+    find_word(all_arrays)
+
+def xml_to_list_dict(filename):
+    data = open(filename,"r")
     contents = data.read()
     contents = contents.replace("   ", "")
     list_of_words = contents.split("\n")
     dict_list = make_word_list(list_of_words, "<entry")
     dict_list = delete_not_word(dict_list)
-
     list_item_nr = 0
     for k in dict_list:
         dict_list[list_item_nr] = make_word_list(k, '<sense level="0"')
@@ -14,18 +36,27 @@ def open_dict(file_name):
     diction_arr = dict_creator(dict_list)
     diction_arr = ref_in_id_to_sense0(diction_arr)
 
-    #for k in diction_arr:
-        #print(k)
+    true_arr_list = tru_arr_creator(diction_arr)
+    return(true_arr_list)
 
-    true_arr_list = tru_arr_creator(diction_arr )
+def write_to_file(list, filename):
+    with open(filename, 'w') as filehandle:
+        for listitem in list:
+            k = filehandle.write('%s\n' % listitem)
+    print('file', filename, 'with', k,'lines has been created' )
 
-    #for k in true_arr_list:
-    #    print(k)
+def read_finished_file(filename):
+    words = []
+    # open file and read the content in a list
+    with open('listfile.txt', 'r') as filehandle:
+        for line in filehandle:
+            # remove linebreak which is the last character of the string
+            currentPlace = line[:-1]
+            words.append(ast.literal_eval(currentPlace))
 
-    #word_find = input("which word you want to find? : ")
-    #print("KK")
-    #print(next(item for item in true_arr_list if item["word"] == word_find))
-    #input()
+    return(words)
+
+def find_word(true_arr_list):
     while True:
         try:
             word_find=input("Type the word you want to find? ")
@@ -34,9 +65,9 @@ def open_dict(file_name):
                 for xx in slowo["translit"]:
                     for x in (xx["translation"]):
                         print(x)
-                print('')
+                print(slowo)
             except:
-                print("there is no word")
+                print("There is no word")
         except EOFError:
             break
 
@@ -51,7 +82,6 @@ def tru_arr_creator(diction_arr):
         while k < len(diction_arr[list_item_nr]["sense_0"]):
             translit_arr.append(sense0_modifier(diction_arr[list_item_nr]["sense_0"][k]))
             k = k + 1
-        #print("b")
 
         for item in diction_arr[list_item_nr]["id_word"]:
             if len(item) > len("<pos>"):
@@ -61,18 +91,14 @@ def tru_arr_creator(diction_arr):
                 else:
                     pos = ""
         true_arr ={
-        "id":  diction_arr[list_item_nr]["id_word"][0],
-        "word": diction_arr[list_item_nr]["id_word"][2],
-        "pron": diction_arr[list_item_nr]["id_word"][3],
-        "pos": pos,
+        "id":  word_modifier(diction_arr[list_item_nr]["id_word"][0],'<entry xml:id="', '>"'),
+        "word": word_modifier(diction_arr[list_item_nr]["id_word"][2], '<orth>', '</orth>' ),
+        "pron": word_modifier(diction_arr[list_item_nr]["id_word"][3],  '<pron>', '</pron>' ),
+        "pos": word_modifier(pos, '<pos>', '</pos>' ),
         "translit": translit_arr
         }
         list_item_nr = list_item_nr + 1
         true_arr_list.append(true_arr)
-    true_arr_list = array_modifier(true_arr_list, "id", '<entry xml:id="', '>"' )
-    true_arr_list = array_modifier(true_arr_list, "word", '<orth>', '</orth>' )
-    true_arr_list = array_modifier(true_arr_list, "pron", '<pron>', '</pron>' )
-    true_arr_list = array_modifier(true_arr_list, "pos", '<pos>', '</pos>' )
 
     k = 0
     while (k < len(true_arr_list)):
@@ -82,45 +108,64 @@ def tru_arr_creator(diction_arr):
         k = k + 1
     return(true_arr_list)
 
-# deleting all unneccery char elements
-def array_modifier(true_arr_list, key, item_front_del, item_back_del ):
-    item_nr = 0
-    while (item_nr  < len(true_arr_list)):
-        if (true_arr_list[item_nr][key] != ""):
-            true_arr_list[item_nr][key] = true_arr_list[item_nr][key][len(item_front_del):]
-            true_arr_list[item_nr][key] = true_arr_list[item_nr][key][:-len(item_back_del)]
-        item_nr = item_nr + 1
-    return(true_arr_list)
-
 def sense0_modifier(sense_list):
     translate = {
-                "pos": "",
-                "translation": [],
-                "english_m": {
+                    "pos": "",
+                    "translation": [],
+                    "english_m":{
                                 "eng": "",
+                                "idarex": "",
                                 "translation": []
+                                },
+
                 }
-    }
     #print(sense_list)
-    word0 = '<note type="idarex" xml:lang="en">'
-    word0_back = '</note>'
+    ref = '<ref>'
+    ref_back = '</ref>'
+
+    word0 = '<quote xml:lang="en">'
+    word0_back = "</quote>"
+
+    word01 = '<note type="idarex" xml:lang="en">'
+    word01_back = '</note>'
+
     word1 = "<quote>"
     word1_back = "</quote>"
+
     word2 = '<pos>'
     word2_back = '</pos>'
 
     item = 0
+    cit_nr = 0
+    word_status = 0
     while (item < len(sense_list)):
+
+        if (cit_nr == 0):
+            word_status = 0
+
+        if (word_status == 1):
+            if (sense_list[item][0:len('<cit')] == '<cit'):
+                cit_nr = cit_nr + 1
+            elif (sense_list[item] == '</cit>'):
+                cit_nr = cit_nr - 1
+            elif (sense_list[item][0:len(word1)] == word1):
+                translate["english_m"]["translation"].append(word_modifier(sense_list[item], word1, word1_back))
+                word_status = 0
+            elif (len(sense_list[item]) > len(word01)):
+                if (sense_list[item][0:len(word01)] == word01):
+                    translate["english_m"]["idarex"] = word_modifier(sense_list[item], word01, word01_back)
+
+        if (len(sense_list[item]) > len(ref)):
+            if (sense_list[item][0:len(ref)] == ref):
+                translate["translation"].append(word_modifier(sense_list[item], ref, ref_back))
+
+
         if (len(sense_list[item]) > len(word0)):
             if (sense_list[item][0:len(word0)] == word0):
                 translate["english_m"]["eng"] = word_modifier(sense_list[item], word0, word0_back)
+                word_status = 1
+                cit_nr = cit_nr + 1
 
-                while (item < len(sense_list)):
-                    if (len(sense_list[item]) > len(word1)):
-                        if (sense_list[item][0:len(word1)] == word1):
-                            translate["english_m"]["translation"].append(word_modifier(sense_list[item], word1, word1_back))
-                    item = item +1
-                break
         if (len(sense_list[item]) > len(word1)):
             if (sense_list[item][0:len(word1)] == word1):
                 translate["translation"].append(word_modifier(sense_list[item], word1, word1_back))
